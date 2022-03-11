a21386f093bc9811bf73acfb5a2aa587ea861f11
rumbo <rumbo.9370@gmail.com>
2022-03-11 08:37:27 +0100

    beskriveAfDatatype

---
+++ variabler-opg/GPOpgaver/beskriveAfDatatype/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+int Sizeof = sizeof(int);
+/*int.MaxValue*/
+WriteLine($"int er {Sizeof} bytes stor");
+WriteLine($"int kan holde {int.MaxValue} værdier");
+DiagramOfDataTypsSize();
+
+
+void DiagramOfDataTypsSize()
+{
+    for (int i = 0; i < Sizeof; i++)
+    {
+        WriteLine("|------------|");
+        WriteLine("|            |");
+        WriteLine("|   1 byet   |");
+        WriteLine("|            |");
+        WriteLine("|------------|");
+
+        WriteLine(" ");
+    }
+}
